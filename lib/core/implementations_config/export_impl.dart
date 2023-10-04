bac517a257be904dbb2ba52b5852e58ed7e92a14
chrnotag <felippehouse@gmail.com>
2023-10-03 23:07:04 -0300

    SPAPC-881 ALTERAÇÃO NOME DO APLICATIVO

---
+++ lib/core/implementations_config/export_impl.dart
@@ -0,0 +1,10 @@
+export 'dart:convert';
+export 'dart:developer';
+
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:modular_study/core/constants/endpoints.dart';
+
+export '../../../models/exceptions_responses/exception_model.dart';
+export 'api_response.dart';
+export 'error_response.dart';
+export 'sucess_response.dart';
