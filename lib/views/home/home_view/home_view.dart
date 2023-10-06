f68a2b6b055ddb5736d48e1643993d3512f05d0e
sotolani.consultoria <sotolani.consultoria@wefin.com.br>
2023-10-06 11:18:53 -0300

    SPACP-861

--- lib/views/home/home_view/home_view.dart
+++ lib/views/home/home_view/home_view.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:modular_study/core/constants/extensions/theme_extensions.dart';
+import 'package:modular_study/core/constants/route_labels.dart';
+import 'package:modular_study/core/constants/themes/theme_configs.dart';
 import 'package:modular_study/core/providers/auth_provider_config/auth_providers.dart';
+import 'package:modular_study/generated/assets.dart';
 import 'package:modular_study/widgets/appbar_logo_perfil.dart';
 import 'package:modular_study/widgets/botao_selecao_empresa.dart';
 
@@ -8,7 +14,6 @@ class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _HomeViewState createState() => _HomeViewState();
 }
 
@@ -16,61 +21,149 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     final AuthProvider authProvider = Modular.get<AuthProvider>();
+    double spacing = 20; // Espaço entre os widgets
+    double totalSpacing = 3 * spacing; // Espaço total entre os widgets
+    double widthCard = (MediaQuery.of(context).size.width - totalSpacing) / 3;
+    double heigthCard = widthCard * 1.3;
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: AppBar().preferredSize,
-        child: const AppBarLogo(),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: AppBarLogo(),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(children: [
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
             SelecaoEmpresa(
-                nomeEmpresa: authProvider.empresaSelecionada!.nome,
-                changeble: true),
-            // Expanded(child: Container()),
-            GridView.count(
-              // Definindo apenas uma "coluna" para criar um layout de 1 linha
-              crossAxisCount: 1,
-              children: <Widget>[
-                // Espaço vazio
-                Container(color: Colors.transparent),
-                Container(color: Colors.red, height: 100),
-                Container(color: Colors.green, height: 100),
-                Container(color: Colors.blue, height: 100),
-              ],
-
+              nomeEmpresa: authProvider.empresaSelecionada!.nome,
+              changeble: true,
             ),
-            // Row(
-            //   children: [
-            //     Container(
-            //       height: 50,
-            //       child: ListView(
-            //         scrollDirection: Axis.horizontal,
-            //         shrinkWrap: true,
-            //         children: [
-            //           Container(
-            //             width: 50,
-            //             height: 50,
-            //             color: Colors.red,
-            //           ),
-            //           Container(
-            //             width: 50,
-            //             height: 50,
-            //             color: Colors.red,
-            //           ),
-            //           Container(
-            //             width: 50,
-            //             height: 50,
-            //             color: Colors.red,
-            //           ),
-
-            //         ],
-            //       ),
-            //     ),
-            //   ],
-            // )
-          ]),
+            Flexible(child: Container()),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () =>
+                        Modular.to.pushNamed(AppRoutes.assinaturaDigitalRoute),
+                    child: Card(
+                      child: Container(
+                        height: heigthCard,
+                        width: widthCard,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SvgPicture.asset(
+                                    Assets.iconsIcAssinatura,
+                                    color: AppColors.botaoEnvio,
+                                  ),
+                                  CircleAvatar(
+                                    radius: 10,
+                                    backgroundColor: Colors.red,
+                                    child: Text(
+                                      '1',
+                                      style: context.textTheme.bodySmall!
+                                          .copyWith(color: Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 14,
+                              ),
+                              Text(
+                                'Assinatura Digital',
+                                style: context.textTheme.bodyLarge!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.globalBackground),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () =>
+                        Modular.to.pushNamed(AppRoutes.monitorOperacoesRoute),
+                    child: Card(
+                      child: Container(
+                        height: heigthCard,
+                        width: widthCard,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(
+                                LineIcons.areaChart,
+                                color: AppColors.botaoEnvio,
+                                size: 40,
+                              ),
+                              SizedBox(
+                                height: 14,
+                              ),
+                              Text(
+                                'Monitor de Operação',
+                                style: context.textTheme.bodyLarge!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.globalBackground),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () =>
+                        Modular.to.pushNamed(AppRoutes.helpScreenRoute),
+                    child: Card(
+                      child: Container(
+                        height: heigthCard,
+                        width: widthCard,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SvgPicture.asset(
+                                    Assets.iconsIcAtendimento,
+                                    fit: BoxFit.fill,
+                                    color: AppColors.botaoEnvio,
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 14,
+                              ),
+                              Text(
+                                'Central de Atendimento',
+                                style: context.textTheme.bodyLarge!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.globalBackground),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
