544790369841345b1def305d9019ad5c843e4a90
felippe.consultoria <felippe.consultoria@wefin.com.br>
2024-01-30 13:07:36 -0300

    master

---
+++ lib/core/constants/contatos.dart
@@ -0,0 +1,7 @@
+class Contatos{
+  static const telefoneSrm = '0800 878 1005';
+  static const emailSrm = 'recebiveis@srmasset.com';
+
+  static const telefoneTrust = '0800 878 8338';
+  static const emailTrust = 'atendimento@trusthub.com.br';
+}
