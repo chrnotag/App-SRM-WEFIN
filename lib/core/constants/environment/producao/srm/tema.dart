85fd9dfd4fae120049c00f6bd463166daead33c5
felippe.consultoria <felippe.consultoria@wefin.com.br>
2024-04-10 08:54:31 -0300

    tudo ok

--- lib/core/constants/environment/producao/srm/tema.dart
+++ lib/core/constants/environment/producao/srm/tema.dart
@@ -15,55 +15,54 @@ class ThemeSRM {
   }
 
   static ThemeData get theme => ThemeData(
-        primaryColor: AppColors.verdePrimarioTRUST,
-        indicatorColor: AppColors.verde,
-        focusColor: AppColors.verdePrimarioTRUST,
-        searchBarTheme: SearchBarThemeData(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.r)),
-                    side:
-                        const BorderSide(color: AppColors.verdePrimarioTRUST))),
-            constraints:
-                BoxConstraints.expand(width: double.infinity, height: 35.h),
-            hintStyle: MaterialStateProperty.all<TextStyle>(
-                GoogleFonts.montserrat(
-                    fontSize: AppSizes().bodyMedium,
-                    color: AppColors.labelText)),
-            textStyle:
-                MaterialStateProperty.all<TextStyle>(GoogleFonts.montserrat(
-              fontSize: AppSizes().bodySmall,
-            ))),
-        colorScheme: ColorScheme(
-            brightness: Brightness.light,
-            primary: AppColors.verdePrimarioTRUST,
-            onPrimary: Colors.white,
-            secondary: AppColors.verdePrimarioTRUST,
-            onSecondary: AppColors.verdePrimarioTRUST,
-            error: AppColors.vermelho,
-            onError: Colors.redAccent.shade200,
-            background: Colors.white,
-            onBackground: AppColors.verdePrimarioTRUST,
-            surface: Colors.white,
-            onSurface: Colors.black,
-            inverseSurface: Colors.black),
-        appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.transparent,
-            shadowColor: Colors.transparent,
-            iconTheme: IconThemeData(color: AppColors.verdePrimarioTRUST)),
-        scaffoldBackgroundColor: Color(0xfff1f1f1),
-        backgroundColor: Colors.white,
-        textTheme: TextTheme(
-          bodySmall: GoogleFonts.montserrat(fontSize: AppSizes().bodySmall),
-          bodyMedium: GoogleFonts.montserrat(fontSize: AppSizes().bodyMedium),
-          bodyLarge: GoogleFonts.montserrat(fontSize: AppSizes().bodyLarge),
-          labelMedium: GoogleFonts.montserrat(fontSize: AppSizes().labelMedium),
-          displaySmall:
-              GoogleFonts.montserrat(fontSize: AppSizes().displaySmall),
-          displayMedium:
-              GoogleFonts.montserrat(fontSize: AppSizes().displayMedium),
-        ),
-      );
+    primaryColor: AppColors.azulPrimarioSRM,
+    indicatorColor: AppColors.azul,
+    focusColor: AppColors.laranjaSRM,
+    searchBarTheme: SearchBarThemeData(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                side: BorderSide.none)),
+        constraints:
+        BoxConstraints.expand(width: double.infinity, height: 50.h),
+        hintStyle: MaterialStateProperty.all<TextStyle>(
+            GoogleFonts.montserrat(
+                fontSize: AppSizes().bodyLarge,
+                color: AppColors.labelText)),
+        textStyle:
+        MaterialStateProperty.all<TextStyle>(GoogleFonts.montserrat(
+          fontSize: AppSizes().bodyLarge,
+        ))),
+    dialogBackgroundColor: Colors.white,
+    colorScheme: ColorScheme(
+        brightness: Brightness.light,
+        primary: AppColors.azulPrimarioSRM,
+        onPrimary: Colors.white,
+        secondary: Colors.white,
+        onSecondary: Colors.white,
+        error: AppColors.vermelho,
+        onError: Colors.redAccent.shade200,
+        background: AppColors.azulPrimarioSRM,
+        onBackground: AppColors.azulPrimarioSRM,
+        surface: Colors.white,
+        onSurface: Colors.black,
+        inverseSurface: Colors.white),
+    appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.white)),
+    scaffoldBackgroundColor: AppColors.azul,
+    textTheme: TextTheme(
+      bodySmall: GoogleFonts.montserrat(fontSize: AppSizes().bodySmall),
+      bodyMedium: GoogleFonts.montserrat(fontSize: AppSizes().bodyMedium),
+      bodyLarge: GoogleFonts.montserrat(fontSize: AppSizes().bodyLarge),
+      labelMedium: GoogleFonts.montserrat(fontSize: AppSizes().labelMedium),
+      displaySmall:
+      GoogleFonts.montserrat(fontSize: AppSizes().displaySmall),
+      displayMedium:
+      GoogleFonts.montserrat(fontSize: AppSizes().displayMedium),
+    ),
+  );
   static String get logo => 'assets/images/logo_srm.png';
   static String get logoAppBar => 'assets/images/logo_srm.png';
   static Widget get imagemAjuda =>
