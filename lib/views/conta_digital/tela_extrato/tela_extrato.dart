6211774a0721bbbb557aac5b3548e3326b041525
felippe.consultoria <felippe.consultoria@wefin.com.br>
2024-04-26 09:16:27 -0300

    trocando de branch

---
+++ lib/views/conta_digital/tela_extrato/tela_extrato.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TelaExtrato extends StatefulWidget {
+  const TelaExtrato({super.key});
+
+  @override
+  State<TelaExtrato> createState() => _TelaExtratoState();
+}
+
+class _TelaExtratoState extends State<TelaExtrato> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
