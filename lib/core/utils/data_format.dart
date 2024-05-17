231579ffb2245a86e5301c76d65f63e27990c426
felippe.consultoria <felippe.consultoria@wefin.com.br>
2024-05-17 17:14:05 -0300

    corrigido

--- lib/core/utils/data_format.dart
+++ lib/core/utils/data_format.dart
@@ -5,13 +5,11 @@ class FormatarData{
     return data.substring(0,10);
   }
   static String formatarExtenso(DateTime data) {
-    var diaDaSemana = DateFormat('EEEE', 'pt_BR').format(data);
-    var dia = DateFormat('dd', 'pt_BR').format(data);
-    var mes = DateFormat('MMMM', 'pt_BR').format(data);
-    var ano = DateFormat('yyyy', 'pt_BR').format(data);
+    var diaDaSemana = DateFormat("EEEE, dd 'de' MMMM 'de' yyyy ", "pt-BR").format(data);
+
 
     diaDaSemana = diaDaSemana.replaceAll('-feira', '');
 
-    return '$diaDaSemana, $dia de $mes de $ano';
+    return diaDaSemana;
   }
 }
