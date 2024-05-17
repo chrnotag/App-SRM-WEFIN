3b4c0de84c392d9c59d02ace13cbd403aae80645
felippe.consultoria <felippe.consultoria@wefin.com.br>
2024-05-17 11:06:53 -0300

    funcionalidades adicionadas e flavor atualizado

--- lib/core/utils/data_format.dart
+++ lib/core/utils/data_format.dart
@@ -4,4 +4,14 @@ class FormatarData{
   static String formatar(String data){
     return data.substring(0,10);
   }
+  static String formatarExtenso(DateTime data) {
+    var diaDaSemana = DateFormat('EEEE', 'pt_BR').format(data);
+    var dia = DateFormat('dd', 'pt_BR').format(data);
+    var mes = DateFormat('MMMM', 'pt_BR').format(data);
+    var ano = DateFormat('yyyy', 'pt_BR').format(data);
+
+    diaDaSemana = diaDaSemana.replaceAll('-feira', '');
+
+    return '$diaDaSemana, $dia de $mes de $ano';
+  }
 }
