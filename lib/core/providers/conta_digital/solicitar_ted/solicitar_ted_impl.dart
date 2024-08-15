8c0efc2705676f26e6fe7d10ac2a2355d52680d7
felippe.consultoria <felippe.consultoria@wefin.com.br>
2024-08-15 17:26:29 -0300

    feito

--- lib/core/providers/conta_digital/solicitar_ted/solicitar_ted_impl.dart
+++ lib/core/providers/conta_digital/solicitar_ted/solicitar_ted_impl.dart
@@ -15,19 +15,14 @@ class SolicitarTedRespostaImpl {
     final ambiente = Modular.get<Environment>();
     final Uri url = Uri.parse(ambiente.endpoints.solicitarTedContaDigital);
     final body = json.encode(parametro.toJson());
-    print('solicitando ted');
-    print('CURL:\nurl: $url\nbody: $body\n header: ${header()}');
-
     try {
       final response = await http.post(url, headers: header(), body: body);
       if (response.statusCode == 200) {
         return SucessResponse(null);
       } else {
-        print('retorno ted: ${response.body}');
         return MensagemErroPadrao.erroResponse(response.bodyBytes);
       }
-    } catch (e, s) {
-      print('erro solicitar ted $e,$s');
+    } catch (_) {
       return MensagemErroPadrao.codigo500();
     }
   }
@@ -35,20 +30,15 @@ class SolicitarTedRespostaImpl {
   static Future<ApiResponse<dynamic>> enviarToken() async {
     final ambiente = Modular.get<Environment>();
     final Uri url = Uri.parse(ambiente.endpoints.tokenSolicitacaoTed);
-    print('CURL: \n url: $url\nheader: ${header()}');
-
     try {
       final response = await http.post(url, headers: header());
-      print('resposta token: ${response.body}');
       if (response.statusCode == 200) {
         final data = RespostaTokenModel.fromJson(json.decode(response.body));
         return SucessResponse(data);
       } else {
-        print('erro dando aqui: ${response.body}');
         return MensagemErroPadrao.erroResponse(response.bodyBytes);
       }
-    } catch (e, s) {
-      print('erro solicitar ted $e,$s');
+    } catch (_) {
       return MensagemErroPadrao.codigo500();
     }
   }
--- lib/widgets/popup_generico.dart
+++ lib/widgets/popup_generico.dart
@@ -1,56 +1,76 @@
+import 'package:Srm_Asset/core/constants/classes_abstratas/envirioment.dart';
 import 'package:flutter/material.dart';
 import 'package:Srm_Asset/core/constants/extensions/screen_util_extension.dart';
 import 'package:Srm_Asset/core/constants/extensions/theme_extensions.dart';
 import 'package:Srm_Asset/widgets/wefin_patterns/wefin_default_button.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../core/constants/configs_tema/srm/colors.dart';
 
 class AlertDialogGenerico extends StatefulWidget {
-  final IconData? icon;
   final String title;
   final String msg;
   final VoidCallback onPressed;
   final Widget? onPressedSecond;
 
-  const AlertDialogGenerico(
-      {super.key,
-      required this.title,
-      required this.msg,
-      required this.onPressed,
-      this.onPressedSecond,
-      this.icon});
+  const AlertDialogGenerico({super.key,
+    required this.title,
+    required this.msg,
+    required this.onPressed,
+    this.onPressedSecond,});
 
   @override
   State<AlertDialogGenerico> createState() => _AlertDialogGenericoState();
 }
 
 class _AlertDialogGenericoState extends State<AlertDialogGenerico> {
+  final ambiente = Modular.get<Environment>();
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      icon: widget.icon != null ? Icon(
-        widget.icon,
-        size: 100.r,
-        color: context.focusColor,
-      ) : Container(),
-      title: Column(
-        children: [
-          Text(
-            widget.title,
-            style: context.textTheme.labelMedium,
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10.h, bottom: 20.h),
-            child: Text(
-              widget.msg,
-              style: context.textTheme.bodyMedium,
+        title: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset(ambiente.alerta_icone),
+                Padding(
+                  padding:
+                  EdgeInsets.symmetric(vertical: 50.h),
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.title,
+                        style: context.textTheme
+                            .displaySmall!
+                            .copyWith(
+                            color:
+                            SRMColors.textBodyColor,
+                            fontWeight:
+                            FontWeight.w900),
+                      ),
+                      Text(
+                        widget.msg,
+                        style: context.textTheme.bodyLarge!
+                            .copyWith(
+                            color: context
+                                .labelTextColor),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 20.h,),
+                      BotaoPadrao(
+                          label: 'Fechar', onPressed: widget.onPressed)
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-          BotaoPadrao(label: 'OK', onPressed: widget.onPressed),
-          Padding(
-            padding: EdgeInsets.only(top: 10.h),
-            child: widget.onPressedSecond ?? Container(),
-          )
-        ],
-      ),
+        ),
     );
   }
 }
